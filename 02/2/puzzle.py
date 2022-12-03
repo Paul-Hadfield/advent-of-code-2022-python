b00047835f9e96a4c591065359fffd674ed9d1b2
Paul Hadfield <paulhadfield@gmail.com>
2022-12-03 20:44:08 +0000

    Day 2, part 2

---
+++ 02/2/puzzle.py
@@ -0,0 +1,77 @@
+def parsePlayer1PlayType(encodedPlayType):
+    if encodedPlayType == 'A':
+        return 'Rock'
+    if encodedPlayType == 'B':
+        return 'Paper'
+    if encodedPlayType == 'C':
+        return 'Scissors'
+    raise NameError('Unknown value')
+
+def parsePlayer2PlayType(player1PlayType, encodedPlayType):
+    if encodedPlayType == 'X':
+        if player1PlayType == 'Rock':
+            return 'Scissors'
+        elif player1PlayType == 'Paper':
+            return 'Rock'
+        else:
+            return "Paper"
+    if encodedPlayType == 'Y':
+        return player1PlayType
+    if encodedPlayType == 'Z':
+        if player1PlayType == 'Rock':
+            return 'Paper'
+        elif player1PlayType == 'Paper':
+            return 'Scissors'
+        else:
+            return "Rock"
+    raise NameError('Unknown value')
+
+def parseGameMove(gameLine):
+    player1 = parsePlayer1PlayType(gameLine[0])
+    return {"player1": player1, "player2": parsePlayer2PlayType(player1, gameLine[2])}
+
+def getScore(player, winner, played):
+    
+    if player == winner:
+        winScore = 6
+    elif winner == 0:
+        winScore = 3
+    else:
+        winScore = 0
+
+    if played == 'Rock':
+        playedScore = 1
+    elif played == 'Paper':
+        playedScore = 2
+    else:
+        playedScore = 3
+
+    return winScore + playedScore
+
+def playGame(game):
+    if game["player1"] == game["player2"]:
+        winner = 0
+# Rock defeats Scissors, Scissors defeats Paper, and Paper defeats Rock
+    elif game["player1"] == 'Rock' and game["player2"] == 'Scissors':
+         winner = 1
+    elif game["player1"] == 'Scissors' and game["player2"] == 'Paper':
+         winner = 1
+    elif game["player1"] == 'Paper' and game["player2"] == 'Rock':
+         winner = 1
+    else:
+        winner = 2
+
+    return {
+        "player1Score": getScore(1, winner, game["player1"]),
+        "player2Score": getScore(2, winner, game["player2"]) 
+    }
+
+def getPlayer2Scores(result):
+    return result["player2Score"]
+
+with open('data.txt') as f:
+    games = list(map(parseGameMove, f.read().splitlines()))
+    results =  list(map(playGame, games))
+    player2Score = list(map(getPlayer2Scores, results))
+
+print(sum(player2Score))
